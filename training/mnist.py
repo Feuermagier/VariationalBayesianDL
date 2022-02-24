7970787aec7c1176129f55a33fab19fe59385f04
Florian Seligmann <floseligmann@web.de>
2022-02-24 14:53:56 +0100

    ensembles

---
+++ training/mnist.py
@@ -0,0 +1,27 @@
+import numpy as np
+import torch
+import torchvision
+from torchvision.transforms import transforms
+import matplotlib.pyplot as plt
+
+def _flatten(x):
+    return torch.flatten(x)
+
+flattening_transform = transforms.Compose([
+  transforms.ToTensor(),
+  transforms.Normalize(0,5, 0.5),
+  transforms.Lambda(_flatten)
+])
+
+def flattened_trainloader(batch_size: int = 5):
+    dataset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=flattening_transform)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+def flattened_testloader(batch_size: int = 5):
+    dataset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=flattening_transform)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+def imshow(img):
+    img = img / 2 + 0.5     # denormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
--- training/toy.py
+++ training/toy.py
@@ -3,13 +3,27 @@ import numpy as np
 
 # See arXiv:1502.05336 (also used in arXiv:1612.01474)
 class CubicToyDataset(torch.utils.data.IterableDataset):
-
-    def __init__(self, min: float = -4, max: float = 4, sample_count: int = 20, rng = None):
+    def __init__(self, min: float = -4, max: float = 4, sample_count: int = 20, normalize: bool = True, noise: float = 3, rng=None):
         super().__init__()
         if rng is None:
             rng = np.random.default_rng()
-        xs = rng.uniform(min, max, sample_count).astype(np.float32)
-        self.samples = [(torch.tensor([x], dtype=torch.float), torch.tensor([x**3 + rng.normal(0, 9)], dtype=torch.float)) for x in xs]
-    
+
+        if normalize:
+            self.x_norm = 1 / np.abs(max)
+            self.y_norm = 1 / np.abs(max ** 3)
+        else:
+            self.x_norm = 1
+            self.y_norm = 1
+
+        xs, ys = _sample_from_fn(
+            rng, lambda x: x**3, min, max, sample_count, noise)
+        xs, ys = torch.unsqueeze(xs, -1) * self.x_norm, torch.unsqueeze(ys, -1) * self.y_norm
+        self.samples = [(x, y) for x, y in zip(xs, ys)] # Just using zip() doesn't work for some reason
+
     def __iter__(self):
         return iter(self.samples)
+
+def _sample_from_fn(rng: np.random.Generator, function, min, max, sample_count, noise_sigma):
+    xs = rng.uniform(min, max, sample_count)
+    ys = function(xs) + rng.normal(loc=0, scale=noise_sigma, size=sample_count)
+    return torch.from_numpy(xs).float(), torch.from_numpy(ys).float()
