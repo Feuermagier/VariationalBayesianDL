'Fix' BBB by including KL rescaling (which is included as a bug in the Blundell repro)

experiments/mnist.py
@@ -22,22 +22,22 @@ flattening_transform = transforms.Compose([
 
 def trainloader(batch_size: int = 5, shuffle: bool = True) -> torch.utils.data.DataLoader:
     dataset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
-    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
 def testloader(batch_size: int = 5, shuffle: bool = True) -> torch.utils.data.DataLoader:
     dataset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
     return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
 
-def flattened_trainloader(batch_size: int = 5, shuffle: bool = True) -> torch.utils.data.DataLoader:
-    dataset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=flattening_transform)
-    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
+def fashion_trainloader(batch_size: int = 5, shuffle: bool = True) -> torch.utils.data.DataLoader:
+    dataset = torchvision.datasets.FashionMNIST(root="./data", train=True, download=True, transform=transform)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
-def flattened_testloader(batch_size: int = 5, shuffle: bool = True) -> torch.utils.data.DataLoader:
-    dataset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=flattening_transform)
+def fashion_testloader(batch_size: int = 5, shuffle: bool = True) -> torch.utils.data.DataLoader:
+    dataset = torchvision.datasets.FashionMNIST(root="./data", train=False, download=True, transform=transform)
     return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
 
 def imshow(img):
-    img = img.reshape((IMAGE_SIZE, IMAGE_SIZE, 1))
+    #img = img.reshape((IMAGE_SIZE, IMAGE_SIZE, 1)) # unflatten
     img = img / 2 + 0.5     # denormalize
     npimg = img.numpy()
     plt.imshow(npimg, cmap=plt.get_cmap('gray'))

experiments/multiclass_classification.py
@@ -12,21 +12,10 @@ from bayesian_torch.models.dnn_to_bnn import dnn_to_bnn, get_kl_loss
 
 from training import util
 from training.swag import SWAGWrapper
-from training.bbb import run_bbb_epoch, BBBLinear, GaussianPrior
+from training.bbb import BBBConvolution, run_bbb_epoch, BBBLinear, GaussianPrior
 from training.calibration import reliability_diagram
 
 def eval_model(name, eval_fn, losses, samples, testloader, device):
-    fig = plt.figure(figsize=(15, 5))
-    fig.suptitle(name, fontsize=16)
-    epochs = len(losses)
-
-    # Plot loss over time
-    loss_ax = fig.add_subplot(1, 2, 1)
-    loss_ax.set_xlabel("Epochs", fontsize=14)
-    loss_ax.set_xticks(np.arange(1, epochs + 1, 1))
-    loss_ax.set_ylabel("Training Loss", fontsize=14)
-    loss_ax.plot(np.arange(1, epochs + 1, 1), losses)
-
     # Test performance
     errors = torch.empty(0)
     confidences = torch.empty(0)
@@ -38,13 +27,24 @@ def eval_model(name, eval_fn, losses, samples, testloader, device):
             errors = torch.cat((errors, preds == target))
             confs = outputs[:,torch.arange(outputs.shape[1]),preds].mean(dim=0).exp()
             confidences = torch.cat((confidences, confs))
-    print(f"Test Accuracy: {errors.sum() / len(errors)}")
+    accuracy = errors.sum() / len(errors)
+
+    fig = plt.figure(figsize=(15, 5))
+    fig.suptitle(name + f" (Test Accuracy {accuracy:.3f})", fontsize=16)
+    epochs = len(losses)
+
+    # Plot loss over time
+    loss_ax = fig.add_subplot(1, 2, 1)
+    loss_ax.set_xlabel("Epochs", fontsize=14)
+    loss_ax.set_xticks(np.arange(1, epochs + 1, 1))
+    loss_ax.set_ylabel("Training Loss", fontsize=14)
+    loss_ax.plot(np.arange(1, epochs + 1, 1), losses)
 
     rel_ax = fig.add_subplot(1, 2, 2)
     reliability_diagram(10, errors, confidences, rel_ax)
 
 def point_predictor(layers, epochs, dataloader, batch_size, device):
-    pp_model = util.generate_model(layers, "relu", "logsoftmax")
+    pp_model = util.generate_model(layers)
     pp_model.to(device)
 
     optimizer = torch.optim.SGD(pp_model.parameters(), lr=0.01)
@@ -62,7 +62,7 @@ def point_predictor(layers, epochs, dataloader, batch_size, device):
         epoch_loss /= (len(dataloader) * batch_size)
         losses.append(epoch_loss)
         if epoch % 1 == 0:
-            print(f"Epoch {epoch}: loss {epoch_loss}")
+            print(f"Epoch {epoch}: NLL loss {epoch_loss}")
     print(f"Final loss {epoch_loss}")
 
     def eval_pp(input, samples):
@@ -71,7 +71,7 @@ def point_predictor(layers, epochs, dataloader, batch_size, device):
     return eval_pp, losses
 
 def swag(layers, epochs, dataloader, batch_size, swag_config, device):
-    swag_model = util.generate_model(layers, "relu", "logsoftmax")
+    swag_model = util.generate_model(layers)
     swag_model.to(device)
     optimizer = torch.optim.SGD(swag_model.parameters(), lr=0.01)
     wrapper = SWAGWrapper(swag_model, swag_config, device)
@@ -90,12 +90,13 @@ def swag(layers, epochs, dataloader, batch_size, swag_config, device):
         epoch_loss /= (len(dataloader) * batch_size)
         losses.append(epoch_loss)
         if epoch % 1 == 0:
-            print(f"Epoch {epoch}: loss {epoch_loss}")
+            print(f"Epoch {epoch}: NLL loss {epoch_loss}")
     print(f"Final loss {epoch_loss}")
+    wrapper.report_status()
 
     def eval_swag(input, samples):
         torch.manual_seed(42)
-        return [wrapper.sample(swag_model, input) for _ in range(samples)]
+        return [wrapper.sample(input) for _ in range(samples)]
     
     return eval_swag, losses
 
@@ -116,7 +117,7 @@ def ensemble(ensemble_count, layers, epochs, dataloader, batch_size):
                 epoch_loss += loss
             if epoch % 100 == 0:
                 print(f"  Epoch {epoch}: loss {epoch_loss / (len(dataloader) * batch_size)}")
-        print(f"  Final loss {epoch_loss / (len(dataloader) * batch_size)}")
+        print(f"Final loss {epoch_loss / (len(dataloader) * batch_size)}")
 
 
     def eval_esemble(input, samples):
@@ -149,14 +150,16 @@ def mc_droupout(p, layers, epochs, dataloader, batch_size):
 
     return eval_dropout
 
-def bbb(prior, mc_sample, sampling, layers, epochs, dataloader, batch_size, device):
-    bbb_model = util.generate_model(layers, "relu", "sigmoid", linear_fn=lambda i, o: BBBLinear(i, o, prior, prior, device, mc_sample=mc_sample, sampling=sampling))
+def bbb(prior, sampling, mc_samples, layers, epochs, dataloader, batch_size, device):
+    linear_fn = lambda i, o: BBBLinear(i, o, prior, prior, device, sampling=sampling)
+    conv_fn = lambda i, o, k: BBBConvolution(i, o, k, prior, prior, device, sampling=sampling)
+    bbb_model = util.generate_model(layers, "relu", "sigmoid", linear_fn=linear_fn, conv_fn=conv_fn)
     bbb_model.to(device)
     optimizer = torch.optim.SGD(bbb_model.parameters(), lr=0.01)
     loss_fn = torch.nn.BCELoss()
 
     for epoch in range(epochs):
-        loss = run_bbb_epoch(bbb_model, optimizer, loss_fn, dataloader, device)
+        loss = run_bbb_epoch(bbb_model, optimizer, loss_fn, dataloader, device, samples=mc_samples)
         if epoch % 100 == 0:
             print(f"Epoch {epoch}: loss {loss / (len(dataloader) * batch_size)}")
     print(f"Final loss {loss / (len(dataloader) * batch_size)}")

experiments/toy_regression.py
@@ -172,7 +172,7 @@ def mc_dropout(p, layers, noise, learn_var, epochs, dataloader, batch_size):
     return eval_dropout
 
 def intel_bbb(layers, noise, learn_var, epochs, dataloader, batch_size):
-    intel_model = util.GaussWrapper(util.generate_model(layers, "relu", None), noise, learn_var)
+    intel_model = util.GaussWrapper(util.generate_model(layers), noise, learn_var)
     dnn_to_bnn(intel_model, {
             "prior_mu": 0.0,
             "prior_sigma": 1.0,
@@ -205,14 +205,14 @@ def intel_bbb(layers, noise, learn_var, epochs, dataloader, batch_size):
 
     return eval_bayes
 
-def bbb(layers, noise, learn_var, epochs, dataloader, batch_size, device, sampling="activations", layer_samples=1, global_samples=5, prior=GaussianPrior(0, 1)):
+def bbb(layers, noise, learn_var, epochs, dataloader, batch_size, device, sampling="activations", layer_samples=1, global_samples=5, kl_rescaling=1, prior=GaussianPrior(0, 1)):
     pi = 0.25  # 0.25, 0.5, 0.75
     sigma1 = np.exp(-0)  # 0, 1, 2
     sigma2 = np.exp(-6)  # 6, 7, 8
     #prior = util.GaussianMixture(pi, sigma1, sigma2)
 
-    bbb_model = util.GaussWrapper(util.generate_model(layers, "relu", None, 
-        linear_fn=lambda i, o: BBBLinear(i, o, prior, prior, device, mc_sample=layer_samples, sampling=sampling)), 
+    bbb_model = util.GaussWrapper(
+        util.generate_model(layers, linear_fn=lambda i, o: BBBLinear(i, o, prior, prior, device, mc_sample=layer_samples, sampling=sampling)), 
         noise, learn_var)
     bbb_model.to(device)
     optimizer = torch.optim.SGD(bbb_model.parameters(), lr=0.001, momentum=0.95)
@@ -221,7 +221,7 @@ def bbb(layers, noise, learn_var, epochs, dataloader, batch_size, device, sampli
         return F.gaussian_nll_loss(mean, target, var)
 
     for epoch in range(epochs):
-        loss = run_bbb_epoch(bbb_model, optimizer, uncurried_nll_loss, dataloader, device, samples=global_samples)
+        loss = run_bbb_epoch(bbb_model, optimizer, uncurried_nll_loss, dataloader, device, samples=global_samples, kl_rescaling=kl_rescaling)
         if epoch % 10 == 0:
             print(f"Epoch {epoch}: loss {loss / (len(dataloader) * batch_size)}")
     print(f"Final loss {loss / (len(dataloader) * batch_size)}")

training/bbb.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from socket import NI_MAXSERV
 import numpy as np
 import torch
 import torch.nn as nn
@@ -11,6 +10,7 @@ from .util import GaussianMixture
 
 def run_bbb_epoch(model: nn.Sequential, optimizer: torch.optim.Optimizer, loss_fn, loader: torch.utils.data.DataLoader, device: torch.device, **kwargs) -> torch.Tensor:
     samples = kwargs.get("samples", 1)
+    kl_rescaling = kwargs.get("kl_rescaling", 1)
     model.train()
     epoch_loss = torch.tensor(0, dtype=torch.float)
     for i, (data, target) in enumerate(loader):
@@ -26,7 +26,7 @@ def run_bbb_epoch(model: nn.Sequential, optimizer: torch.optim.Optimizer, loss_f
         for _ in range(samples):
             output = model(data)
             kl = sum([getattr(layer, "kl", 0) for layer in model])
-            loss += pi * kl + loss_fn(output, target)
+            loss += pi * kl + loss_fn(output, target) * kl_rescaling
         loss /= samples
         loss.backward()
         #torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
@@ -43,8 +43,6 @@ class BBBLinear(nn.Module):
         self.mc_sample = kwargs.get("mc_sample", 1)
         self.freeze_on_eval = kwargs.get("freeze_on_eval", True)
         self.device = device
-        self.out_features, self.in_features = out_features, in_features
-
         self.in_features, self.out_features = in_features, out_features
         self.weight_prior, self.bias_prior = weight_prior, bias_prior
 
@@ -104,12 +102,66 @@ class BBBLinear(nn.Module):
         else:
             raise ValueError("Invalid value of sampling")
 
+class BBBConvolution(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, weight_prior, bias_prior, device: torch.device, **kwargs):
+        super().__init__()
+        self.is_bayesian = True
+        self.sampling = kwargs.get("sampling", "activations")
+        self.stride = kwargs.get("stride", 1)
+        self.freeze_on_eval = kwargs.get("freeze_on_eval", True)
+        self.device = device
+        self.out_channels, self.in_channels = out_channels, in_channels
+        self.kernel_size = kernel_size
+
+        self.weight_prior, self.bias_prior = weight_prior, bias_prior
+
+        # Weights
+        self.weight_mu = nn.Parameter(torch.empty((self.out_channels, self.in_channels, self.kernel_size, self.kernel_size)).normal_(0, 0.1))
+        self.weight_rho = nn.Parameter(torch.empty((self.out_channels, self.in_channels, self.kernel_size, self.kernel_size)).uniform_(-3, -3))
+
+        # Biases
+        self.bias_mu = nn.Parameter(torch.empty(self.out_channels).normal_(0, 0.1))
+        self.bias_rho = nn.Parameter(torch.empty(self.out_channels).uniform_(-3, -3))
+
+        self.kl = 0
+
+    def sample_parameters(self, mu, rho):
+        epsilon = torch.empty(rho.shape).normal_(0, 1).to(self.device)
+        return mu + to_sigma(rho) * epsilon
+
+    def forward(self, input: torch.Tensor):
+        self.kl = 0
+
+        if self.sampling == "parameters":
+            raise NotImplementedError()
+        elif self.sampling == "activations":
+            activation_mu = F.conv2d(input, self.weight_mu, self.bias_mu, self.stride)
+            activation_var = F.conv2d(input**2, to_sigma(self.weight_rho)**2, to_sigma(self.bias_rho)**2, self.stride)
+            activation_std = torch.sqrt(activation_var)
+            
+            #log_prior = self.weight_prior.log_prob(self.weight_mu).sum() + self.bias_prior.log_prob(self.bias_mu).sum() 
+            #self.kl = -log_prior
+            weight_kl = self.weight_prior.kl_divergence(self.weight_mu, to_sigma(self.weight_rho))
+            bias_kl = self.bias_prior.kl_divergence(self.bias_mu, to_sigma(self.bias_rho))
+            self.kl = weight_kl + bias_kl
+
+            if not self.training and self.freeze_on_eval:
+                epsilon = torch.empty(activation_mu.shape[1:]).normal_(0, 1).unsqueeze(0).expand((activation_mu.shape)).to(self.device)
+            else:
+                epsilon = torch.empty(activation_mu.shape).normal_(0, 1).to(self.device)
+            output = activation_mu + activation_std * epsilon
+            
+            return output
+        else:
+            raise ValueError("Invalid value of sampling")
+
 def to_sigma(rho):
     return F.softplus(rho)
 
 def log_prob(mu, rho, value):
-    sigma = to_sigma(rho)
-    return torch.clamp(-((value - mu)**2) / (2 * sigma**2) - sigma.log() - math.log(math.sqrt(2 * math.pi)), -23, 0)
+    #sigma = to_sigma(rho)
+    #return torch.clamp(-((value - mu)**2) / (2 * sigma**2) - sigma.log() - math.log(math.sqrt(2 * math.pi)), -23, 0)
+    return torch.clamp(torch.distributions.Normal(mu, to_sigma(rho), False).log_prob(value), -23, 0)
 
 # Closed form KL divergence for gaussians
 # See https://github.com/kumar-shridhar/PyTorch-BayesianCNN/blob/master/metrics.py

training/bbb_blundell.py
@@ -132,7 +132,7 @@ class BayesianNetwork(nn.Module):
             lpq += self.layers[i].lqw
         return lpw, lpq
 
-    def BBB_loss(self, input, target, batch_idx = None):
+    def BBB_loss(self, input, target, variance, batch_idx = None):
 
         s_log_pw, s_log_qw, s_log_likelihood, sample_log_likelihood = 0., 0., 0., 0.
         for _ in range(self.SAMPLES):
@@ -141,7 +141,7 @@ class BayesianNetwork(nn.Module):
             if self.CLASSES > 1:
                 sample_log_likelihood = -F.nll_loss(output, target, reduction='sum')
             else:
-                sample_log_likelihood = -(.5 * (target - output) ** 2).sum()
+                sample_log_likelihood = -F.gaussian_nll_loss(output, target, variance)
             s_log_pw += sample_log_pw
             s_log_qw += sample_log_qw
             s_log_likelihood += sample_log_likelihood
@@ -150,6 +150,6 @@ class BayesianNetwork(nn.Module):
 
         # KL weighting
         if batch_idx is None: # standard literature approach - Graves (2011)
-            return (1. / (self.NUM_BATCHES)) * (l_qw - l_pw) - l_likelihood
+            return (1. / (self.NUM_BATCHES)) * (l_qw - l_pw) - 3 * l_likelihood
         else: # alternative - Blundell (2015)
             return 2. ** ( self.NUM_BATCHES - batch_idx - 1. ) / ( 2. ** self.NUM_BATCHES - 1 ) * (l_qw - l_pw) - l_likelihood

training/swag.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
+from torch.nn.utils.convert_parameters import parameters_to_vector, vector_to_parameters
 import torch.nn.functional as F
 import math
+import numpy as np
 
 @dataclass
 class SwagConfig:
@@ -18,7 +20,7 @@ class SWAGWrapper:
         self.start_epoch = config.get("start_epoch", 0)
         self.model = model
 
-        self.weights = torch.nn.utils.convert_parameters.parameters_to_vector(self.model.parameters())
+        self.weights = parameters_to_vector(self.model.parameters())
         self.sq_weights = self.weights**2
         self.updates = 0
         self.deviations = torch.zeros((self.weights.shape[0], self.deviation_samples)).to(device)
@@ -42,15 +44,21 @@ class SWAGWrapper:
             print(f"SWAG: starting to collect samples at epoch {epoch}, batch {batch_idx}")
         if epoch >= self.start_epoch and (epoch + 1) % self.update_every_epochs == 0 and (batch_idx + 1) % self.update_every_batches == 0:
             self.updates += 1
-            params = torch.nn.utils.convert_parameters.parameters_to_vector(self.model.parameters())
+            params = parameters_to_vector(self.model.parameters())
             self.weights = (self.updates * self.weights + params) / (self.updates + 1)
             self.sq_weights = (self.updates * self.sq_weights + params**2) / (self.updates + 1)
             self.deviations = torch.roll(self.deviations, -1, 1)
             self.deviations[:,-1] = params - self.weights
             self.param_dist_valid = False
 
-    def sample(self, model: nn.Module, input: torch.Tensor):
+    def sample(self, input: torch.Tensor):
+        old_params = parameters_to_vector(self.model.parameters())
         self.update_param_dist()
         weight_sample = self.param_dist.sample()
-        torch.nn.utils.convert_parameters.vector_to_parameters(weight_sample, model.parameters())
-        return model(input)
+        vector_to_parameters(weight_sample, self.model.parameters())
+        output = self.model(input)
+        vector_to_parameters(old_params, self.model.parameters())
+        return output
+
+    def report_status(self):
+        print(f"SWAG: Collected {np.minimum(self.updates, self.deviation_samples)} out of {self.deviation_samples} deviation samples and {self.updates} parameter samples")

training/util.py
@@ -46,18 +46,32 @@ def map_activation(name):
     else:
         raise ValueError(f"Unknown activation function {name}")
 
-def generate_model(architecture, activation, out_activation, scale=1, linear_fn=lambda i, o: nn.Linear(i, o), dropout_p=0):
+def generate_model(architecture, scale=1, linear_fn=lambda i, o: nn.Linear(i, o), conv_fn=lambda i, o, k: nn.Conv2d(i, o, k), dropout_p=0):
     layers = []
-    for i, (in_features, out_features) in enumerate(architecture):
-        in_features_scaled = in_features if i == 0 else int(in_features * scale)
-        out_features_scaled = out_features if i == len(architecture) - 1 else int(out_features * scale)
-        layers.append(linear_fn(int(in_features_scaled), int(out_features_scaled)))
-        if i < len(architecture) - 1:
-            layers.append(map_activation(activation))
-            if dropout_p > 0:
+    for i, (ty, size) in enumerate(architecture):
+        if ty == "pool":
+            layers.append(nn.MaxPool2d(size))
+        elif ty == "flatten":
+            layers.append(nn.Flatten())
+        elif ty == "relu":
+            layers.append(nn.ReLU())
+        elif ty == "sigmoid":
+            layers.append(nn.Sigmoid())
+        elif ty == "logsoftmax":
+            layers.append(nn.LogSoftmax(dim=1))
+        elif ty == "fc":
+            (in_features, out_features) = size
+            in_features_scaled = in_features if i == 0 else int(in_features * scale)
+            out_features_scaled = out_features if i == len(architecture) - 1 else int(out_features * scale)
+            layers.append(linear_fn(int(in_features_scaled), int(out_features_scaled)))
+            if i < len(architecture) - 1 and dropout_p > 0:
                 layers.append(nn.Dropout(dropout_p))
-        elif out_activation is not None:
-            layers.append(map_activation(out_activation))
+        elif ty == "conv":
+            (in_channels, out_channels, kernel_size) = size
+            layers.append(conv_fn(in_channels, out_channels, kernel_size))
+        else:
+            raise ValueError(f"Unknown layer type '{ty}'")
     model = nn.Sequential(*layers)
     print(f"Generated model: {model}")
+    print(f"{sum([p.numel() for p in model.parameters() if p.requires_grad])} trainable parameters")
     return model