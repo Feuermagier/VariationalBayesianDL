Fix ECE calculation and add a cyclic lr schedule for SWAG

experiments/multiclass_classification.py
@@ -74,7 +74,7 @@ def swag(layers, epochs, dataloader, batch_size, swag_config, device):
     swag_model = util.generate_model(layers)
     swag_model.to(device)
     optimizer = torch.optim.SGD(swag_model.parameters(), lr=0.01)
-    wrapper = SWAGWrapper(swag_model, swag_config, device)
+    wrapper = SWAGWrapper(swag_model, optimizer, swag_config, device)
     losses = []
     for epoch in range(epochs):
         epoch_loss = torch.tensor(0, dtype=torch.float)
@@ -161,10 +161,11 @@ def bbb(prior, sampling, mc_samples, kl_rescaling, layers, epochs, dataloader, b
     losses = []
     for epoch in range(epochs):
         loss = run_bbb_epoch(bbb_model, optimizer, loss_fn, dataloader, device, samples=mc_samples, kl_rescaling=kl_rescaling)
+        loss /= len(dataloader) * batch_size
         losses.append(loss.detach())
         if epoch % 1 == 0:
-            print(f"Epoch {epoch}: loss {loss / (len(dataloader) * batch_size)}")
-    print(f"Final loss {loss / (len(dataloader) * batch_size)}")
+            print(f"Epoch {epoch}: loss {loss}")
+    print(f"Final loss {loss}")
 
 
     def eval_bbb(input, samples):

training/calibration.py
@@ -24,7 +24,11 @@ def reliability_diagram(bin_count, errors, confidences, ax=None):
     for (x, count) in zip(interval, bins):
         ax.text(x + 0.5 * 1 / bin_count, 0.01, str(len(count)), color="white", fontsize=14, ha="center")
 
-    ece = np.mean(np.abs(bin_accuracys - bin_confidences))
+    ece = 0
+    for i in range(bin_count):
+        ece += len(bins[i]) * np.abs(bin_accuracys[i] - bin_confidences[i])
+    ece /= len(confidences)
+
     mce = np.max(np.abs(bin_accuracys - bin_confidences))
 
     ident = [0.0, 1.0]

training/swag.py
@@ -13,12 +13,15 @@ class SwagConfig:
     max_cov_samples: int
 
 class SWAGWrapper:
-    def __init__(self, model: nn.Module, config, device):
+    def __init__(self, model: nn.Module, optimizer, config, device, use_lr_cycles: bool = True):
         self.update_every_batches = config.get("update_every_batches", 1)
-        self.update_every_epochs = config.get("update_every_epochs", 1)
         self.deviation_samples = config.get("deviation_samples", 10)
         self.start_epoch = config.get("start_epoch", 0)
+        self.max_lr = config.get("max_lr", 0.005)
+        self.min_lr = config.get("min_lr", 0.001)
         self.model = model
+        self.optimizer = optimizer
+        self.use_lr_cycles = use_lr_cycles
 
         self.weights = parameters_to_vector(self.model.parameters())
         self.sq_weights = self.weights**2
@@ -26,6 +29,7 @@ class SWAGWrapper:
         self.deviations = torch.zeros((self.weights.shape[0], self.deviation_samples)).to(device)
         self.param_dist_valid = False
         self.param_dist = None
+        self.batches_since_swag_start = 0
 
     @property
     def mean(self):
@@ -40,16 +44,25 @@ class SWAGWrapper:
             self.param_dist_valid = True
 
     def update(self, epoch, batch_idx):
-        if epoch == self.start_epoch and (batch_idx + 1) == self.update_every_batches:
-            print(f"SWAG: starting to collect samples at epoch {epoch}, batch {batch_idx}")
-        if epoch >= self.start_epoch and (epoch + 1) % self.update_every_epochs == 0 and (batch_idx + 1) % self.update_every_batches == 0:
-            self.updates += 1
-            params = parameters_to_vector(self.model.parameters())
-            self.weights = (self.updates * self.weights + params) / (self.updates + 1)
-            self.sq_weights = (self.updates * self.sq_weights + params**2) / (self.updates + 1)
-            self.deviations = torch.roll(self.deviations, -1, 1)
-            self.deviations[:,-1] = params - self.weights
-            self.param_dist_valid = False
+        if epoch >= self.start_epoch:
+            self.batches_since_swag_start += 1
+
+            if self.batches_since_swag_start % self.update_every_batches == 0:
+                self.updates += 1
+                params = parameters_to_vector(self.model.parameters())
+                self.weights = (self.updates * self.weights + params) / (self.updates + 1)
+                self.sq_weights = (self.updates * self.sq_weights + params**2) / (self.updates + 1)
+                self.deviations = torch.roll(self.deviations, -1, 1)
+                self.deviations[:,-1] = params - self.weights
+                self.param_dist_valid = False
+                if self.use_lr_cycles:
+                    print(f"SWAG: Collected a sample at epoch {epoch}, batch {batch_idx} with last lr {self.lr}")
+
+            if self.use_lr_cycles:
+                t = 1 - (self.batches_since_swag_start % self.update_every_batches) / self.update_every_batches
+                self.lr = t * (self.max_lr - self.min_lr) + self.min_lr
+                for g in self.optimizer.param_groups:
+                    g["lr"] = self.lr
 
     def sample(self, input: torch.Tensor):
         old_params = parameters_to_vector(self.model.parameters())