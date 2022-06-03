Add VI for cluster and fix loss computation

experiments/fmnist/fmnist.py
@@ -12,7 +12,7 @@ import experiments.base.multiclass_classification as exp
 from training.util import sgd
 from training.pp import MAP
 from training.ensemble import Ensemble
-from training.bbb import BBBModel
+from training.bbb import BBBModel, GaussianPrior
 from training.swag import SwagModel
 
 def run(device, config, out_path, log):
@@ -40,6 +40,8 @@ def run(device, config, out_path, log):
         trained_model = run_mc_dropout(device, trainloader, config, out_path)
     elif model == "multi_mc_dropout":
         trained_model = run_mc_dropout(device, trainloader, config, out_path)
+    elif model == "mfvi":
+        trained_model = run_mfvi(device, trainloader, config, out_path)
     else:
         raise ValueError(f"Unknown model type '{model}'")
     
@@ -191,6 +193,29 @@ def run_multi_mc_dropout(device, trainloader, config, model_out_path):
     model = Ensemble([MAP(layers) for _ in range(members)])
     model.train_model(config["epochs"], torch.nn.NLLLoss(), sgd(config["lr"]), trainloader, config["batch_size"], device, report_every_epochs=1)
     torch.save(model.state_dict(), model_out_path + f"multi_mc_dropout_{members}.tar")
+    return model
+
+def run_mfvi(device, trainloader, config, model_out_path):
+    prior = GaussianPrior(0, 1)
+    layers = [
+        ("v_conv", (1, 6, 5, prior, {})),
+        ("relu", ()),
+        ("pool", 2),
+        ("v_conv", (6, 16, 5, prior, {})),
+        ("relu", ()),
+        ("pool", 2),
+        ("flatten", ()),
+        ("v_fc", (16 * 4 * 4, 120, prior, {})),
+        ("relu", ()),
+        ("v_fc", (120, 84, prior, {})),
+        ("relu", ()),
+        ("v_fc", (84, 10, prior, {})),
+        ("logsoftmax", ())
+    ]
+
+    model = BBBModel(layers)
+    model.train_model(config["epochs"], torch.nn.NLLLoss(), sgd(config["lr"]), trainloader, config["batch_size"], device, mc_samples=config["mc_samples"], kl_rescaling=config["kl_rescaling"], report_every_epochs=1)
+    torch.save(model.state_dict(), model_out_path + f"mfvi.tar")
     return model
 
 ####################### CW2 #####################################

training/bbb.py
@@ -39,29 +39,29 @@ class BBBModel(nn.Module):
                 data, target = data.to(device), target.to(device)
                 optimizer.zero_grad()
 
-                kl_loss = torch.tensor(0, dtype=torch.float)
-                data_loss = torch.tensor(0, dtype=torch.float)
+                kl_loss = torch.tensor(0, dtype=torch.float, device=data.device)
+                data_loss = torch.tensor(0, dtype=torch.float, device=data.device)
                 for _ in range(mc_samples):
                     output = self.model(data)
                     kl_loss += sum([getattr(layer, "kl", 0) for layer in self.model]) / data.shape[0]
                     data_loss += data_loss_fn(output, target)
 
-                kl_loss /= mc_samples
-                kl_loss *= pi
-                data_loss /= mc_samples
-
-                kl_loss.backward(retain_graph=True)
+                loss = (pi * kl_loss + data_loss) / mc_samples
+                loss.backward()
+                # kl_loss /= mc_samples
+                # kl_loss *= pi
+                # data_loss /= mc_samples
+                # kl_loss.backward(retain_graph=True)
                 #kl_grad = torch.cat([module.rho_grads() for module in self.model if hasattr(module, "rho_grads")])
                 #kl_grads.append(kl_grad)
-
-                data_loss.backward()
+                # data_loss.backward()
                 #data_grad = torch.cat([module.rho_grads() for module in self.model if hasattr(module, "rho_grads")])
                 #data_grad -= kl_grad
                 #data_grads.append(data_grad)
 
                 #nn.utils.clip_grad.clip_grad_norm_(self.model.parameters(), 10)
                 optimizer.step()
-                epoch_loss += pi * kl_loss + data_loss
+                epoch_loss += loss.cpu().item()
             epoch_loss /= (len(loader) * batch_size)
             self.losses.append(epoch_loss.detach())
             if report_every_epochs > 0 and epoch % report_every_epochs == 0:

training/network.py
@@ -6,7 +6,7 @@ import numpy as np
 import itertools
 
 from training.util import GaussLayer
-from training.bbb_layers import BBBLinear, LowRankBBBLinear
+from training.bbb_layers import BBBLinear, LowRankBBBLinear, BBBConvolution
 from training.dropout import FixableDropout
 
 def map_activation(name):
@@ -46,6 +46,9 @@ def generate_model(architecture, print_summary=False):
         elif ty == "conv":
             (in_channels, out_channels, kernel_size) = size
             layers.append(nn.Conv2d(in_channels, out_channels, kernel_size))
+        elif ty == "v_conv":
+            (in_channels, out_channels, kernel_size, prior, args) = size
+            layers.append(BBBConvolution(in_channels, out_channels, kernel_size, prior, prior, **args))
         elif ty == "dropout":
             p, = size
             layers.append(FixableDropout(p))

training/pp.py
@@ -32,7 +32,7 @@ class MAP(nn.Module):
                 loss = loss_fn(output, target)
                 loss.backward()
                 optimizer.step()
-                epoch_loss += loss.cpu()
+                epoch_loss += loss.cpu().item()
             epoch_loss /= (len(loader) * batch_size)
             self.losses.append(epoch_loss.detach())
             if report_every_epochs > 0 and epoch % report_every_epochs == 0:

training/rms.py
@@ -42,7 +42,7 @@ class RMSModule(nn.Module):
                 loss = data_loss + self.reg_scale * reg_loss
                 loss.backward()
                 optimizer.step()
-                epoch_loss += loss.cpu()
+                epoch_loss += loss.cpu().item()
             epoch_loss /= (len(loader) * batch_size)
             self.losses.append(epoch_loss.detach())
             if report_every_epochs > 0 and epoch % report_every_epochs == 0:

training/sgld.py
@@ -47,7 +47,7 @@ class SGLDModule(nn.Module):
                 loss = loss_fn(output, target) * len(loader) * data.shape[-1]
                 loss.backward()
                 optimizer.step()
-                epoch_loss += loss.cpu()
+                epoch_loss += loss.cpu().item()
             epoch_loss /= (len(loader) * batch_size)
             self.losses.append(epoch_loss.detach())
 

training/swag.py
@@ -65,7 +65,7 @@ class SwagModel(nn.Module):
                 loss = loss_fn(output, target)
                 loss.backward()
                 optimizer.step()
-                epoch_loss += loss.cpu()
+                epoch_loss += loss.cpu().item()
                 self.swag_update(epoch, batch_idx, optimizer)
             epoch_loss /= (len(loader) * batch_size)
             self.losses.append(epoch_loss.detach())

training/util.py
@@ -106,7 +106,7 @@ class GaussLayer(nn.Module):
 def plot_losses(name, losses, ax):
     epochs = max([len(loss) for loss in losses])
     ax.set_xlabel("Epoch", fontsize=14)
-    ax.set_xticks(np.arange(0, epochs + 1, epochs // 10))
+    ax.set_xticks(np.arange(0, epochs + 1, epochs // 10 if epochs > 10 else 1))
     ax.set_ylabel("Training Loss", fontsize=14)
     if len(losses) > 1:
         for i, single_losses in enumerate(losses):