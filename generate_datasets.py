8d56e5f887a3760d7cdcb05af42875a884ff2d16
Florian Seligmann <floseligmann@web.de>
2022-06-03 14:30:10 +0200

    Download progress indicator for UCI

--- generate_datasets.py
+++ generate_datasets.py
@@ -54,5 +54,6 @@ datasets = {
 for name, url in datasets.items():
     filename = url.split('/')[-1]
     if not Path(DATA_PATH + "UCI/" + filename).exists():
+        print(f"Downloading {name}...")
         urllib.request.urlretrieve(url, DATA_PATH + "UCI/" + filename)
     zipfile.ZipFile(DATA_PATH + "UCI/CCPP.zip").extractall(DATA_PATH + "UCI/")
